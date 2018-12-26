96921bddc3ff1d927386ba082fadbb922eba1577
renhongdi <renhongdi@mobike.com>
2018-12-26 16:50:18 +0800

    rename to gravity-admin

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM docker.mobike.io/database/nginx:1.15.1-alpine
-COPY dist/drc-admin /usr/share/nginx/html
+COPY dist/gravity-admin /usr/share/nginx/html
 COPY drc.conf /etc/nginx/conf.d/default.conf
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to drc-admin!');
+    expect(page.getParagraphText()).toEqual('Welcome to gravity-admin!');
   });
 });
